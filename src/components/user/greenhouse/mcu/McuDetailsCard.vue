6bdc7329f6965337274c874c1a41efe1ad3bef8d
i4nizer <iansandoval264@gmail.com>
2025-03-26 18:08:15 +0800

    Implemented mcu settings

---
+++ src/components/user/greenhouse/mcu/McuDetailsCard.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <v-icon>mdi-chip</v-icon>
+            <span class="ml-2">Details</span>
+        </v-card-title>
+        <v-card-subtitle>Manage your microcontroller settings and preferences.</v-card-subtitle>
+        <v-card-text>
+            <v-form 
+                class="border-none pa-1"
+                v-model="state.valid" 
+                @submit.prevent="onSubmit"
+            >
+                <v-text-field
+                    label="Name"
+                    v-model="mcu.name"
+                    :rules="[required(), min(3), max(100)]"
+                ></v-text-field>
+                <v-text-field
+                    label="Label"
+                    v-model="mcu.label"
+                ></v-text-field>
+                <v-text-field
+                    readonly
+                    label="Key"
+                    v-model="mcu.key"
+                    append-inner-icon="mdi-content-copy"
+                    @click:append-inner="onCopyKey"
+                ></v-text-field>
+                <v-btn
+                    type="submit"
+                    text="Save"
+                    :disabled="!state.valid || !changed"
+                ></v-btn>
+            </v-form>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup>
+import { useRules } from '@/composables/rules.composable';
+import equal from 'fast-deep-equal';
+import { computed, reactive } from 'vue';
+
+
+
+// ---events
+const emit = defineEmits(['change'])
+
+// ---props
+const props = defineProps({
+    mcu: {
+        type: Object,
+        required: true,
+    }
+})
+
+// ---composables
+const { required, min, max } = useRules()
+
+// ---data
+const mcu = reactive({
+    name: props.mcu?.name,
+    label: props.mcu?.label,
+    key: props.mcu?.key,
+})
+
+// ---getters
+const propsGreenhouse = computed(() => ({
+    name: props.mcu?.name,
+    label: props.mcu?.label,
+    key: props.mcu?.key,
+}))
+const changed = computed(() => !equal(mcu, propsGreenhouse.value))
+
+// ---state
+const state = reactive({ valid: false })
+
+// ---events
+const onSubmit = () => emit('change', { ...props.mcu, ...mcu })
+
+const onCopyKey = async () => await navigator.clipboard.writeText(mcu.key).catch(console.error)
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/user/greenhouse/mcu/McuSettings.vue
+++ src/views/user/greenhouse/mcu/McuSettings.vue
@@ -2,17 +2,57 @@
     <McuLayout>
         <v-container class="pa-5 py-7" fluid>
             <v-row>
-                <v-col>Microcontroller Settings</v-col>
+                <v-col>
+                    <h3>Microcontroller Settings</h3>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <McuDetailsCard 
+                        :mcu="mcu"
+                        :loading="state.updating"
+                        :disabled="state.updating"
+                        @change="onUpdateMcu"
+                    />
+                </v-col>
             </v-row>
         </v-container>
     </McuLayout>
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue';
+import { useMcuStore } from '@/stores/mcu.store';
+import { computed, defineAsyncComponent, reactive } from 'vue';
+import { useRoute } from 'vue-router';
 
 const McuLayout = defineAsyncComponent(() => import("@/views/user/greenhouse/mcu/McuLayout.vue"))
+const McuDetailsCard = defineAsyncComponent(() => import("@/components/user/greenhouse/mcu/McuDetailsCard.vue"))
 
+
+// ---stores
+const { mcus, updateMcu } = useMcuStore()
+
+// ---composables
+const route = useRoute()
+
+// ---data
+const mcuId = route.params.mcuId
+
+// ---getters
+const mcu = computed(() => mcus.find(m => m.id == mcuId))
+
+// ---state
+const state = reactive({ updating: false })
+
+// ---events
+const onUpdateMcu = async (mcu) => {
+    state.updating = true
+
+    await updateMcu(mcu)
+        .catch(console.error)
+
+    state.updating = false
+}
 
 
 </script>
